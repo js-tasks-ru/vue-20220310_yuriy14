53cf7f192a398cb5f44099f94ac7b0e147a3862f
Юрий Яровиков <505-565@mail.ru>
2022-03-23 00:56:38 +0500

    02-components 01-06 tasks

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,11 +2,20 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-
   // Компонент должен иметь входной параметр
-
+  props: {
+    count: {
+      default: 0,
+    },
+  },
+  emits: ['update:count'],
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    buttonClick() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" v-on:click="buttonClick">{{ count }}</button>`,
 });
